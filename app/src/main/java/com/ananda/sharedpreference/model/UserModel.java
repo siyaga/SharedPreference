9becb8bd96ec3c7f48a8e3b1fec754c42f1a90b9
Adi Riyanto <siyaga@users.noreply.github.com>
2020-05-01 14:46:46 +0700

    Deskripsi Pengerjaan    :Membuat User Model

---
+++ app/src/main/java/com/ananda/sharedpreference/model/UserModel.java
@@ -0,0 +1,45 @@
+package com.ananda.sharedpreference.model;
+/*
+Tanggal Pengerjaan      : Jumat, 1 Mei 2020
+Deskripsi Pengerjaan    : Membuat User Model
+NIM                     : Ananda Marwanaya Putra
+Nama                    : 10117157
+Kelas                   : IF-4
+
+ */
+
+public class UserModel {
+
+    private String username;
+    private String password;
+    private String phone;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+
+
+
+}
