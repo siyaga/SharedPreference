3e29a834358ac8afeed36eecb070b004b073d2b1
Adi Riyanto <siyaga@users.noreply.github.com>
2020-05-01 14:47:10 +0700

    Deskripsi Pengerjaan    :Membuat User Model

---
+++ app/src/main/java/com/ananda/sharedpreference/utils/UtilStatic.java
@@ -0,0 +1,17 @@
+package com.ananda.sharedpreference.utils;
+/*
+Tanggal Pengerjaan      : Jumat, 1 Mei 2020
+Deskripsi Pengerjaan    : Membuat Util Static
+NIM                     : Ananda Marwanaya Putra
+Nama                    : 10117157
+Kelas                   : IF-4
+
+ */
+public interface UtilStatic {
+    String DEFAULT_STRING = "";
+    String STRING_0 = "0";
+    String STRING_NULL = "null";
+    int DEFAULT_INT = 0;
+    boolean DEFAULT_BOOLEAN = false;
+
+}
